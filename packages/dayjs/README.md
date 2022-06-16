36ab330d32172cc414b78bd4d3746c4da868f5ba
陈日兴 <18814233102@163.com>
2022-06-17 00:06:30 +0800

    初始化时间类

---
+++ packages/dayjs/README.md
@@ -0,0 +1,11 @@
+# `moment`
+
+> TODO: description
+
+## Usage
+
+```
+const moment = require('moment');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/dayjs/rollup.config.js
@@ -0,0 +1,51 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import clear from 'rollup-plugin-clear';
+import { terser } from 'rollup-plugin-terser';
+import { DEFAULT_EXTENSIONS } from '@babel/core';
+
+const commonConfig = {
+  input: './lib/index.ts',
+  external: [/@babel\/runtime/],
+  plugins: [
+    clear({
+      targets: ['bundle'],
+    }),
+    resolve(),
+    commonjs(),
+  ],
+};
+
+export default {
+  ...commonConfig,
+  input: './cjs/index.js',
+  plugins: [
+    ...commonConfig.plugins,
+    babel({
+      // 编译库使用
+      babelHelpers: 'runtime',
+      // 只转换源代码，不转换外部依赖
+      exclude: 'node_modules/**',
+      // babel 默认不支持 ts 需要手动添加
+      extensions: [...DEFAULT_EXTENSIONS],
+    }),
+  ],
+  output: [
+    {
+      name: 'XPBig',
+      file: 'bundle/number.umd.js',
+      format: 'umd',
+      sourcemap: false,
+      exports: 'named',
+    },
+    {
+      name: 'XPBig',
+      file: 'bundle/number.umd.min.js',
+      format: 'umd',
+      sourcemap: false,
+      exports: 'named',
+      plugins: [terser()],
+    },
+  ],
+};
