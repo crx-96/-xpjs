ee78512b52832508f35696d5690a66bc1fd98ae8
陈日兴 <18814233102@163.com>
2022-07-10 19:07:57 +0800

    ..

--- packages/midway-validate/index.d.ts
+++ packages/midway-validate/index.d.ts
@@ -1,5 +1,6 @@
 export * from './dist/index';
 
 declare module '@midwayjs/core/dist/interface' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface MidwayConfig {}
 }
