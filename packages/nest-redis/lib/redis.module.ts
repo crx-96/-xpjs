b43df8553657852417b5f34911cbeaf1d181f42a
陈日兴 <18814233102@163.com>
2022-08-03 00:07:30 +0800

    ..

--- packages/nest-redis/lib/redis.module.ts
+++ packages/nest-redis/lib/redis.module.ts
@@ -1,1 +1,9 @@
-export class RedisModule {}
+import { DynamicModule } from '@nestjs/common';
+import { RedisOptions } from 'ioredis';
+export class RedisModule {
+  static registerRedis(options: RedisOptions): DynamicModule {
+    return {
+      module: RedisModule,
+    };
+  }
+}
--- packages/nest-redis/lib/redis.service.ts
+++ packages/nest-redis/lib/redis.service.ts
@@ -1,3 +1,11 @@
 import Redis from 'ioredis';
+import { RedisBaseInterface } from './interface';
 
-export class RedisService {}
+export class RedisService implements RedisBaseInterface {
+  constructor() {
+    console.log(666);
+  }
+  get(): string {
+    throw new Error('Method not implemented.');
+  }
+}
